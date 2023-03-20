45a97e077e90c74aea77544967be6c1c6178fa03
mine0112 <jeff112112@gmail.com>
2023-03-20 10:00:47 +0800

    no message

--- src/main/java/com/example/java_demo_test/JavaDemoTestApplication.java
+++ src/main/java/com/example/java_demo_test/JavaDemoTestApplication.java
@@ -8,6 +8,9 @@ public class JavaDemoTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaDemoTestApplication.class, args);
+		System.out.println("=============");
+		
 	}
+	
 
 }
